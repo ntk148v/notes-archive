283840b3959a8ee69fcec4a4b2d3daeee515845a
Kien Nguyen <kiennt65@viettel.com.vn>
2021-03-30 15:02:08 +0700

    Init

---
+++ content/about.md
@@ -0,0 +1,5 @@
+---
+title: About these notes
+---
+
+Hi, I'm [Kien Nguyen-Tuan](https://github.com) 👋.
---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: '@kiennt\'s notes',
+  },
+  plugins: [`gatsby-theme-andy`],
+};
