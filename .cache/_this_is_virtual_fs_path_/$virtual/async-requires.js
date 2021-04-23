ce0bf0903c99342378746867efc6510868164690
Kien Nguyen-Tuan <kiennt2609@gmail.com>
2021-04-23 08:02:05 +0000

    publish

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -0,0 +1,7 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---node-modules-gatsby-theme-andy-src-templates-note-js": () => import("./../../../node_modules/gatsby-theme-andy/src/templates/note.js" /* webpackChunkName: "component---node-modules-gatsby-theme-andy-src-templates-note-js" */)
+}
+
---
+++ .cache/sync-requires.js
@@ -0,0 +1,9 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---node-modules-gatsby-theme-andy-src-templates-note-js": preferDefault(require("/home/runner/work/notes/notes/node_modules/gatsby-theme-andy/src/templates/note.js"))
+}
+
